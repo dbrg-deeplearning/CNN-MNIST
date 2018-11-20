d87e5c44be1f8b050f52f48af7daeb4264d1da8e
zhouyirong09 <zhouyirong09@163.com>
2018-11-20 16:15:59 +0800

    添加GPU资源分配方法

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 # 配置gpu可使用情况, 取值可为"-1"或"0"或"0,1"等, 机器有多块gpu时各gpu编号依次为0至m-1
 # "-1"表示不可使用gpu, "0"表示可使用0号gpu, "0,1"表示可同时使用0号和1号gpu
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # 设置使用规则, allow_soft_placement为True时指若程序中指定使用的gpu満负荷，则自动使用可用的gpu
 config = tf.ConfigProto(allow_soft_placement=True)
 # allow_growth为True时指, gpu显存根据需要随程序进行分配, 为False时则程序启动直接分配整块gpu
@@ -17,7 +17,7 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.9
 
 # 读取数据
 mnist = input_data.read_data_sets('MNIST-data', one_hot=True)
-sess = tf.InteractiveSession()
+sess = tf.InteractiveSession(config=config)
 
 
 # 构建cnn网络结构
